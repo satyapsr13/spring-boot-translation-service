a3b14496a9d6bac076fb99ccdba1ed01a3a20168
Satya Prakash <satyaprakash@styava.com>
2025-03-22 09:57:40 +0530

    Added support for swagger

--- postApp/src/main/java/com/satya/postApp/PostApplication.java
+++ postApp/src/main/java/com/satya/postApp/PostApplication.java
@@ -1,11 +1,15 @@
 package com.satya.postApp;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@Tag(name = "Translation API", description = "REST APIs for text translation")
+
 @SpringBootApplication
 public class PostApplication {
-
+// http://localhost:8080/swagger-ui.html
 	public static void main(String[] args) {
 		SpringApplication.run(PostApplication.class, args);
 	}
